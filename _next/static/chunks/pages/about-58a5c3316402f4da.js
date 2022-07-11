c76a341227b5a6c52c7bd3638d13f5d93aedc56d
Siddharth Ray <sidraystudios@gmail.com>
2022-07-11 14:35:19 -0700

    Initial out subtree commit gh-pages

---
+++ _next/static/chunks/pages/about-58a5c3316402f4da.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[521],{8961:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about",function(){return u(7677)}])}},function(n){n.O(0,[523,112,956,677,774,888,179],(function(){return _=8961,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
---
+++ _next/static/chunks/pages/resume-1398abe1259fabd2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[647],{2416:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/resume",function(){return _(8878)}])},8878:function(n,u,_){"use strict";_.r(u);var e=_(7677);u.default=e.default}},function(n){n.O(0,[523,112,956,677,774,888,179],(function(){return u=2416,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
