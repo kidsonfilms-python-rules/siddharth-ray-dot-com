44faf54869d54d34aa0b619cf5f88ff54b383157
Siddharth Ray <sidraystudios@gmail.com>
2022-08-06 15:08:32 -0700

    Final Responsive Version

--- pages/_app.js
+++ pages/_app.js
@@ -7,15 +7,6 @@ import * as React from "react";
 import SEO from '../next-seo.config';
 
 function MyApp({ Component, pageProps }) {
-  React.useEffect(() => {
-    window.onload = function () {
-      var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
-      if (mobile) {
-        alert("This website was designed for Desktop. Visit this on a computer for better view. Better mobile view coming soon!");
-      }
-    }
-  })
-
   return (
     <>
       <DefaultSeo
