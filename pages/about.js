dcb904d32ecd46e728d694168034c0cb6a639587
Siddharth Ray <sidraystudios@gmail.com>
2024-02-01 21:29:39 -0800

    haha

--- pages/about.js
+++ pages/about.js
@@ -35,7 +35,7 @@ export default function About() {
               companyName="DJFlame" 
               location="Folsom, CA" 
               jobTitle="Founder, CEO" 
-              description={(<>DJFlame is a group-streaming and AI-based song mixing and manipulation application. I worked on a cross-platform desktop application for an automated DJ using multiple methods. DJFlame uses Tensorflow-based AI song-selection and a Guest Companion App for Guest Interaction. It was used at multiple public events for beta testing and planned public release Q4 2022. Check out our website at <a href='https://djflame.tech' target="_blank" rel="noreferrer">djflame.tech</a>.</>)} />
+              description={(<>DJFlame is a group-streaming and AI-based song mixing and manipulation application. I worked on a cross-platform desktop application for an automated DJ using multiple methods. DJFlame uses Tensorflow-based AI song-selection and a Guest Companion App for Guest Interaction. It was used at multiple public events for beta testing and planned public release Q4 2024. Check out our website at <a href='https://djflame.tech' target="_blank" rel="noreferrer">djflame.tech</a>.</>)} />
               <ExperienceBlock timeframe="2015 - 2021" 
               companyName="UTSAV Inc" 
               location="Folsom, CA" 
@@ -47,18 +47,18 @@ export default function About() {
               <ExperienceBlock timeframe="2022 - 2026" 
               companyName="Vista del Lago High School" 
               location="High School Diploma"
-              jobTitle="Freshman" />
-              <ExperienceBlock timeframe="2019 - 2022" 
+              jobTitle="Sophomore" />
+              {/* <ExperienceBlock timeframe="2019 - 2022" 
               companyName="Folsom Middle School" 
               jobTitle="GPA: 4.0" />
-            </div>
+            </div> */}
             <div className={styles.competitions}>
               <h2>Competitions</h2>
               <ExperienceBlock timeframe="2021 – now" 
               companyName="FIRST® Tech Challenge"
               location="Team Juice 16236"
               jobTitle="Software Division Lead, Vice Captain" 
-              description="We built a robot using water-jet cut aluminum parts and programmed the robot using Java and tools like TensorFlow. Went to local and international companies to get sponsorships and made publicly used and open-source tools for the FTC community. We were finalists in the 2022 NorCal Regional Championships and won multiple other awards."/>
+              description="We built a robot using water-jet cut aluminum parts and programmed the robot using Java and tools like TensorFlow. Went to local and international companies to get sponsorships and made publicly used and open-source tools for the FTC community. 2022 Norcal Finalists, 2023 Norcal Champions, 2023 Worlds Championships Division Finalists"/>
               <ExperienceBlock timeframe="2016 – 2019" 
               companyName="FIRST® Lego League"
               location="Team Golden State Builders 30208"
@@ -66,6 +66,7 @@ export default function About() {
               description="We built a LEGO-based robot. In our last year won Qualifiers and made 6th place in NorCal Championships."/>
             </div>
           </div>
+        </div>
         </div>
       </main>
 
